6ac15fd3c59be8c1e39f6302b7b1f3c52befb605
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-07 23:19:40 +0600

    some routes added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Blogs from "../../Pages/Blogs/Blogs";
+import Home from "../../Pages/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/blogs',
+                element: <Blogs></Blogs>
+            }
+        ]
     }
 ])
 
